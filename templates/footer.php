8d39bccb13546aff68766bf439785592f7fe364c
AlexCsanyi <alex.csanyi@gmail.com>
2020-02-28 07:06:18 +0000

    started with tenplates + font awesome

---
+++ templates/footer.php
@@ -0,0 +1,7 @@
+ <!-- Jquery, popper, Bootstrap & main scripts -->
+    <script src="./js/jquery.js"></script>
+    <script src="./js/popper.js"></script>
+    <script src="./js/bootstrap.js"></script>
+    <script src="./js/main.js"></script>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.styles.css">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+    <title>SoundWaves</title>
+</head>
+<body>
