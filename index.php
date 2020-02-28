458c7ad234722979514cbdaad4af79ce513fcf07
AlexCsanyi <alex.csanyi@gmail.com>
2020-02-28 06:35:39 +0000

    initial set up, bootstrap, jquery

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.styles.css">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <title>SoundWaves</title>
+</head>
+<body>
+
+    <h1>Hello Sound Waves</h1>
+
+    <!-- Jquery, popper, Bootstrap & main scripts -->
+    <script src="./js/jquery.js"></script>
+    <script src="./js/popper.js"></script>
+    <script src="./js/bootstrap.js"></script>
+    <script src="./js/main.js"></script>
+</body>
+</html>
