92ba63d77cda5e7fd616b833466fda346793fc0b
AlexCsanyi <alex.csanyi@gmail.com>
2020-02-28 07:23:22 +0000

    started adding bootstrap components

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php include_once 'templates/header.php'; ?>
 
-    <h1>Hello Sound Waves</h1>
+<div class="container text-center">
+    <div class="row">
+        <div class="col-sm-5"></div>
+
+        <div class="col-sm-2" id="login">
+           <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone.
+                    </small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Submit</button>
+            </form>
+        </div>
+
+        <div class="col-sm-5"></div>
+    </div>
+</div>
 
 <?php include_once 'templates/footer.php'; ?>
